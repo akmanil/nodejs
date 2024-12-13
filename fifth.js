dd9a97010a58ba7cb1663699ffdd621bb5fdebf4
akmanil <maharanaanil424@gmail.com>
2024-12-13 15:46:12 +0530

    Adding content to existing file using writeFile method of creating a file

--- fifth.js
+++ fifth.js
@@ -9,3 +9,8 @@ fs.open("myFile2.txt" , "w" ,function(err,file){
     if (err) throw err;
     console.log("Saved")
 });
+
+fs.writeFile("MyFile.txt" ,"Added content that will be added using writeFile" ,function(err){
+    if(err) throw err;
+    console.log("Saved");
+})
