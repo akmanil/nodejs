ecf040afc34890277aa03c89fc571dddcc599c57
akmanil <maharanaanil424@gmail.com>
2024-12-16 17:29:08 +0530

    Adding routs in an object

--- eleven.js
+++ eleven.js
@@ -6,9 +6,26 @@ function app(req ,res){
     // res.end()
 }
 
+//store  routes in an object
+var routes ={
+    '/app1' : function app1 (req ,res){
+        res.writeHead(200);
+        res.end("App1");
+    }
+ ,
+
+    '/app1_about' : function about1(req ,res){
+        res.writeHead(200);
+        res.end("App1 About");
+    }
+}
+
 http.createServer((req ,res) =>{
     if(req.url === "/"){
         return app(req,res);
+    }
+    else if (req.url in routes){
+        return routes[req.url](req ,res);
     }
     res.writeHead(404);
     res.end(http.STATUS_CODES[404]);
